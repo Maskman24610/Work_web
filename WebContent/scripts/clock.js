ee756b4cbca007a6ace3617554b900eedb556665
maskman24610 <showong24610@yahoo.com.tw>
2017-09-13 11:40:00 +0800

    FullCalendar Demo練習以及一些小網頁

---
+++ WebContent/scripts/clock.js
@@ -0,0 +1,26 @@
+whenReady(function() {
+	var clock = document.getElementById('clock');
+	var icon = new Image();
+	icon.src = "images/clock-icon.png";
+	
+	function displayTime() {
+		var now = new Date();
+		var hrs = now.getHours(), mins = now.getMinutes();
+		if (mins < 10) mins = "0" + mins;
+		clock.innerHTML = hrs + ':' + mins;
+		setTimeout(displayTime, 60000)
+	}
+	
+	displayTime();
+	
+	clock.draggable = true;
+	
+	clock.ondragstart = function(event) {
+		var event = event || window.event;
+		
+		var dt = event.dataTransfer;
+		
+		dt.setData("Text", Date() + "\n");
+		if (dt.setDragImage) dt.setDragImage(icon, 0, 0);
+	};
+});
